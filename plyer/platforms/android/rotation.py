9ffea0fb73a6541eac252a995825a60292aba9bd
hcl91 <herve.cl.colin@free.fr>
2016-01-13 22:32:35 +0100

    debug

--- plyer/platforms/android/rotation.py
+++ plyer/platforms/android/rotation.py
@@ -20,7 +20,7 @@ class RotationSensorListener(PythonJavaClass):
             Sensor.TYPE_ROTATION_VECTOR)
         self.WindowManager = cast('android.view.WindowManager',activity.getSystemService(Context.WINDOW_SERVICE))
         self.Display = self.WindowManager.getDefaultDisplay()
-        print "HC disp {}".format(self.Display)
+        
         
         # self.value = None
         self.values = [None, None, None]
@@ -43,8 +43,7 @@ class RotationSensorListener(PythonJavaClass):
         worldAxisForDeviceAxisX = SensorManager.AXIS_X
         worldAxisForDeviceAxisY = SensorManager.AXIS_Z
         
-        screenRotation = self.Display.getRotation();
-        print "HC {}".format(screenRotation)
+        screenRotation = self.Display.getRotation()
   
         #Adjust the rotation matrix for the device orientation
    
